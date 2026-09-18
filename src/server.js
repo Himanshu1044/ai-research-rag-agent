2bb0e77cf7bdf016ba3c14b6a92f2c14a497176f
Himanshu1044 <swamihimanshu78@gmail.com>
2026-09-18 19:02:36 +0530

    Prepare backend for Render deployment

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on http://localhost:${PORT}`)
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is listening on port ${PORT}`)
 })
